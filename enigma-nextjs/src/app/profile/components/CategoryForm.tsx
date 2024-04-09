e1255d1e5536eac4e4cc7bea64ead4cd2d4b2e45
S-Mukherjee98 <shubhramukherjee4@gmail.com>
2024-04-10 02:16:17 +0530

    add new category form done with validation

--- enigma-nextjs/src/app/profile/components/CategoryForm.tsx
+++ enigma-nextjs/src/app/profile/components/CategoryForm.tsx
@@ -1,14 +1,31 @@
 "use client";
 
 import React from "react";
-import { Form, Input, Modal } from "antd";
+import { Form, message, Modal } from "antd";
 import { getAntdFieldRequiredRules } from "@/helpers/Validations";
+import axios from "axios";
 
 function CategoryForm({
   showCategoryForm,
   setShowCategoryForm,
   reloadData,
 }: CategoryFormProps) {
+
+const[form]=Form.useForm()
+const[loading,setLoading]=React.useState(false)
+const onFinish = async(values: any) => {
+    try {
+        setLoading(true)
+        await axios.post("/api/categories",values);
+        setShowCategoryForm(false);
+        reloadData();
+    } catch (error:any) {
+        message.error(error.response.data.message || error.message);
+    }
+    finally{
+        setLoading(false);
+    }
+}
   return (
     <Modal
       open={showCategoryForm}
@@ -17,15 +34,23 @@ function CategoryForm({
       title={<h1 className="text-2xl font-semibold text-gray-800">Add New Category</h1>}
       closable={false}
       okText="Save"
+      onOk={()=>{
+        form.submit();
+      }}
+      okButtonProps={
+        {loading,}
+      }
     >
         <hr />
-      <Form layout="vertical" className="flex flex-col gap-5 mt-7">
+      <Form layout="vertical" className="flex flex-col gap-5 mt-7" form={form} onFinish={onFinish}>
         <Form.Item label="Category Name" name="name"
         rules={getAntdFieldRequiredRules("Category Name is Required")}
         >
           <input type="text" />
         </Form.Item>
-        <Form.Item label="Category Description" name="description">
+        <Form.Item label="Category Description" name="description"
+        
+        >
           <textarea />
         </Form.Item>
       </Form>
